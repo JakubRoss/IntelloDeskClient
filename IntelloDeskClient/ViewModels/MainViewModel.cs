4bcef61f8a3978b4b1e16bcca1837f03823bcbe0
Jakub Ros <Jakub.Rosploch@gmail.com>
2025-06-26 18:29:55 +0100

    Create MainViewModel.cs

---
+++ IntelloDeskClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using IntelloDeskClient.DTO;
+using IntelloDeskClient.Views;
+
+namespace IntelloDeskClient.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty] 
+        private object? biezacyWidok;
+
+        public MainViewModel()
+        {
+            PokazStart();
+        }
+
+        public void PokazStart()
+        {
+            BiezacyWidok = new StartView
+            {
+                DataContext = new StartViewModel(this)
+            };
+        }
+
+        public void PokazDokumenty()
+        {
+            BiezacyWidok = new DokumentyListView
+            {
+                DataContext = new DokumentyListViewModel(this)
+            };
+        }
+
+    }
+}
