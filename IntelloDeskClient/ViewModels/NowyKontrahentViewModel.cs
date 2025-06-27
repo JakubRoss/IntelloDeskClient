31464951f4b18e9f8f80b4212e1ff5c16f07c227
Jakub Ros <Jakub.Rosploch@gmail.com>
2025-06-27 17:54:02 +0100

    Create NowyKontrahentView

---
+++ IntelloDeskClient/ViewModels/NowyKontrahentViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelloDeskClient.ViewModels
+{
+    using CommunityToolkit.Mvvm.ComponentModel;
+    using CommunityToolkit.Mvvm.Input;
+    using IntelloDeskClient.DTO;
+    using IntelloDeskClient.Services;
+    using IntelloDeskClient.Views;
+
+    public partial class NowyKontrahentViewModel : ObservableObject
+    {
+        private readonly ApiService _api = new();
+        private readonly MainViewModel _main;
+
+        [ObservableProperty]
+        private string? symbol;
+
+        [ObservableProperty]
+        private string? nazwa;
+        public NowyKontrahentViewModel(MainViewModel main)
+        {
+            _main = main;
+        }
+
+        [RelayCommand]
+        private async Task AddKontrahent()
+        {
+            if (string.IsNullOrWhiteSpace(symbol) || string.IsNullOrWhiteSpace(nazwa))
+            {
+                Anuluj();
+                return;
+            }
+            var dto = new CreateKontrahentDto
+            {
+                Symbol = Symbol.Trim(),
+                Nazwa = Nazwa.Trim()
+            };
+
+            await _api.AddKontrahentAsync(dto);
+            Anuluj();
+        }
+
+        [RelayCommand]
+        private void Anuluj()
+        {
+            _main.BiezacyWidok = new KontrahenciListView
+            {
+                DataContext = new KontrahenciListViewModel(_main)
+            };
+        }
+    }
+
+}
---
+++ IntelloDeskClient/Views/NowyKontrahentView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace IntelloDeskClient.Views
+{
+    /// <summary>
+    /// Interaction logic for NowyKontrahentView.xaml
+    /// </summary>
+    public partial class NowyKontrahentView : UserControl
+    {
+        public NowyKontrahentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
