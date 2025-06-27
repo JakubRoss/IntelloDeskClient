8ae6783ea37e654b18e4b2284deceb79ff48082b
Jakub Ros <Jakub.Rosploch@gmail.com>
2025-06-27 14:38:18 +0100

    Create DokumentyListView

---
+++ IntelloDeskClient/ViewModels/DokumentyListViewModel.cs
@@ -0,0 +1,86 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using IntelloDeskClient.DTO;
+using IntelloDeskClient.Services;
+using IntelloDeskClient.Views;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace IntelloDeskClient.ViewModels
+{
+    public partial class DokumentyListViewModel : ObservableObject
+    {
+        private readonly MainViewModel _main;
+        private readonly ApiService _api = new();
+        public IRelayCommand PokazPozycjeCommand { get; }
+        public IRelayCommand UsunDokumentCommand { get; }
+
+        public DokumentyListViewModel(MainViewModel main)
+        {
+            _main = main;
+            _ = ZaladujAsync();
+
+            //zbadaj sprawe
+            PokazPozycjeCommand = new RelayCommand<object>(param =>
+            {
+                if (param is DokumentPrzyjeciaDto dokument)
+                {
+                    PokazPozycje(dokument);
+                }
+            });
+
+            UsunDokumentCommand = new RelayCommand<object>(param =>
+            {
+                if (param is DokumentPrzyjeciaDto dokument)
+                {
+                    UsunDokument(dokument.Id);
+                }
+            });
+
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<DokumentPrzyjeciaDto> dokumenty = new();
+
+        [ObservableProperty]
+        private DokumentPrzyjeciaDto? selectedDokument;
+
+        [RelayCommand]
+        private async Task ZaladujAsync()
+        {
+            var lista = await _api.GetDokumentyAsync();
+            Dokumenty = new ObservableCollection<DokumentPrzyjeciaDto>(lista);
+        }
+
+        [RelayCommand]
+        private void Powrot()
+        {
+            _main.BiezacyWidok = new StartView
+            {
+                DataContext = new StartViewModel(_main)
+            };
+        }
+
+        [RelayCommand]
+        private void CreateDokumentView()
+        {
+            _main.BiezacyWidok = new NowePrzyjecieView
+            {
+                DataContext = new NowePrzyjecieViewModel(_main)
+            };
+        }
+
+        private void PokazPozycje(DokumentPrzyjeciaDto dokument)
+        {
+            _main.BiezacyWidok = new PozycjaView
+            {
+                DataContext = new PozycjeViewModel(dokument.Pozycje, _main, dokument.Id)
+            };
+        }
+        private async Task UsunDokument(int id)
+        {
+            await _api.DeleteDokumentAsync(id);
+            await ZaladujAsync();
+        }
+    }
+}
---
+++ IntelloDeskClient/Views/DokumentyListView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using IntelloDeskClient.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntelloDeskClient.Views
+{
+    /// <summary>
+    /// Interaction logic for DokumentyListView.xaml
+    /// </summary>
+    public partial class DokumentyListView : UserControl
+    {
+        public DokumentyListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
